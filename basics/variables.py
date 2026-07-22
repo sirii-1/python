676832aa34a1fd6c63c7bc9cd0cbfaac8bb7de0f
Siri Chandana <your-sirii-1-email@example.com>
2026-07-22 23:41:43 +0530

    Add variables example

---
+++ basics/variables.py
@@ -0,0 +1,17 @@
+# variables.py
+
+# Storing different types of values in variables
+
+name = "Siri Chandana"
+age = 19
+college = "IIIT Guwahati"
+branch = "Electronics and Communication Engineering"
+cgpa = 7.32
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
+print("CGPA:", cgpa)
+print("Student:", is_student)
