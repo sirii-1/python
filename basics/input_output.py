218b6c059d9f458b259d100a6678783ee5142159
Siri Chandana <your-sirii-1-email@example.com>
2026-07-23 00:04:26 +0530

    Add input and output examples

---
+++ basics/input_output.py
@@ -0,0 +1,10 @@
+# input_output.py
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+cgpa = float(input("Enter your CGPA: "))
+
+print("\n----- Student Details -----")
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
