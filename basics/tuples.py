aa95c34bd1a11541e0b5875a40081ce9e9ec1385
Siri Chandana <your-sirii-1-email@example.com>
2026-07-23 00:22:35 +0530

    Tuples

---
+++ basics/tuples.py
@@ -0,0 +1,19 @@
+coordinates = (10.0, 20.0, 30.0)
+point = (5, 10)
+
+x = point[0]
+y = point[1]
+print("x:", x)
+print("y:", y)
+
+latitude, longitude, altitude = coordinates
+print("latitude:", latitude)
+print("longitude:", longitude)
+print("altitude:", altitude)
+
+sample_tuple = (1, 2, 3, 2, 4, 2)
+count_twos = sample_tuple.count(2)
+index_of_three = sample_tuple.index(3)
+
+print("count_twos:", count_twos)
+print("index_of_three:", index_of_three)
