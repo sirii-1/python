1bc4375ad55c4737a179b2bb5a74c0a66edcbd28
Siri Chandana <your-sirii-1-email@example.com>
2026-07-23 00:01:44 +0530

    Add data types examples

---
+++ basics/data_types.py
@@ -0,0 +1,45 @@
+# data_types.py
+
+name = "Siri Chandana"
+age = 19
+cgpa = 7.32
+is_student = True
+
+print("Value:", name)
+print("Type:", type(name))
+
+print("Value:", age)
+print("Type:", type(age))
+
+print("Value:", cgpa)
+print("Type:", type(cgpa))
+
+print("Value:", is_student)
+print("Type:", type(is_student))
+
+city = "Hyderabad"
+semester = 3
+attendance = 92.5
+placement_ready = False
+
+print("Value:", city)
+print("Type:", type(city))
+
+print("Value:", semester)
+print("Type:", type(semester))
+
+print("Value:", attendance)
+print("Type:", type(attendance))
+
+print("Value:", placement_ready)
+print("Type:", type(placement_ready))
+
+x = 100
+y = 100.0
+z = "100"
+a = False
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
