db184cfc906602606bb081a214ba86aeb0110074
Siri Chandana <your-sirii-1-email@example.com>
2026-07-23 00:05:46 +0530

    Add type casting examples

---
+++ basics/type_casting.py
@@ -0,0 +1,30 @@
+# operators.py
+
+a = 15
+b = 4
+
+print("Arithmetic Operators")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+print()
+
+print("Comparison Operators")
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+print()
+
+print("Logical Operators")
+print(a > 10 and b < 5)
+print(a > 20 or b < 5)
+print(not(a > 20))
